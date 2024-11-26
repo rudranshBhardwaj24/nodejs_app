768c383281b38543f8874233578679c8c141babb
Rudransh Bhardwaj <249.rudranshbhardwaj@gmail.com>
2024-11-26 23:15:54 +0530

    updated structer added models

--- app.js
+++ app.js
@@ -1,104 +1,36 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const connectDB = require('./src/config/database')
+const User = require('./src/models/user')
 
 const cors = require('cors');
 app.use(cors());
 
-const dbURI = "mongodb://localhost:27017/games"
+const portNumber = 8000
 
 app.use(bodyParser.json())
 
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("Connected")).catch(console.log("Error connecting"))
+//Add user
+app.post('/add-user', async (req, res) => {
+  const newUser = new User(req.body)
 
-
-const userSchema = new mongoose.Schema(
-  {
-    name: String,
-    email: String,
-    password: String
+  try{
+  await newUser.save();
+  res.send("User added successfully!")}
+  catch(err){
+    res.status(401).send("Could not save the user! " + err)
   }
-)
+})
 
-const User = mongoose.model('User', userSchema);
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/user/:username', async (req, res) => {
-  const username = req.params.username
-  const user = await User.findOne({name: username})
-  console.log(user)
-  if(user!=null){
-  res.send(user)}
-  else{
-    res.status(404).send("User not found!")
-  }
-})
-
-app.get('/login/auth', async (req, res) => {
-  const username = req.body.name
-  console.log(username)
-  
-  const password = req.body.password
-  console.log(password)
-  const usernameCheck = await User.findOne({name: username})
-  console.log(usernameCheck)
-  if(username == usernameCheck.name && password == usernameCheck.password){
-    res.send("Login successful!")
-  }
-  else{
-    res.send("User not found")
-  }
-})
-
-app.post('/add-user', async (req, res) => {
-
-  const data = req.body;
-
-  const newUserCheck = await User.findOne({name: req.body.name})
-  console.log(newUserCheck)
-
-  const newProfile = new User({
-    name: data.name,
-    email: data.email,
-    password: data.password
+connectDB().then(()=>{
+  console.log("Database connection established....")
+  app.listen(portNumber, ()=>{
+    console.log("Server started on port number: " + portNumber)
   })
-
-  if(newUserCheck==null){
-  newProfile.save()
-  res.send("user added!")
-  }
-  else{
-    res.send("Username already in use! please use a new username")
-  }
-})
-
-app.get('/login', (req, res) => {
-    res.send('Login Page!')
-})
-
-app.get('/hello', (req, res) => {
-  res.send('Hey!')
-})
-
-app.post('/add', (req, res) => {
-  try{
-    const data = req.body;
-    
-    res.status(200).json({message:"data received successfully", receivedData: data})
-  }
-
-catch{
-  console.error();
-  res.send("Error")
-}})
-
-
-app.post('')
-app.listen(8000)
+}).catch(err => {console.log("Database connection failed....")})
